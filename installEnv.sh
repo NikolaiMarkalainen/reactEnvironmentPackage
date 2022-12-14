3ea2aa4e72a947ed019b5cb850b2625c53c81b3b
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-12-14 10:35:37 +0200

    added npm packages downloads and files

--- installEnv.sh
+++ installEnv.sh
@@ -42,27 +42,26 @@ cd ..
 
 cd src
 
+mkdir components 
+mkdir services
+cd services
+touch request.js
+cd ..
+
 echo -e "import React from 'react'\nimport ReactDOM from 'react-dom/client'\nimport App from './App'\nReactDOM.createRoot(document.getElementById('root')).render(<App/>)\n " > index.js
 
-echo -e "import './App.css';
-import {useEffect, useState} from 'react';
-
-const App = () => {
-  return (
-    <h1>Hello WORLD</h1>
-  );
-}
-
-export default App;" > App.js
-npm install axios
+echo -e "import './App.css';\nimport {useEffect, useState} from 'react';\nconst App = () => {\n  return (\n    <h1>Hello WORLD</h1>\n  );\n}\nexport default App;" > App.js
 
 echo -e "" > App.css
+
+cd ..
+
+npm install axios
 
 echo "Front end setup with axios installed"
 
 sleep 1
 
-cd ..
 cd ..
 cd backend
 
@@ -74,8 +73,15 @@ npm install cors
 npm install dotenv
 touch .env
 echo -e ".env"  | tee -a > .gitignore
+
+
+echo ""
+echo ""
+
 npm install eslint --save-dev
-npx eslint --init
+npx eslint --init --skip-prompts
+
+
 touch .eslintignore
 echo -e "build" > .eslintignore
 npm install --save-dev supertest
@@ -84,4 +90,41 @@ echo ""
 echo ""
 echo "Installed express, nodemon, cors, dotenv, eslint, supertest, express-async-errors"
 
+mkdir tests
+mkdir requests
+mkdir models
+mkdir controllers
+mkdir utils
 
+cd utils
+touch middleware.js
+touch test_helper.js
+touch config.js
+touch logger.js
+
+cd ..
+
+cd tests
+touch dummy.test.js
+
+cd .. 
+
+cd models
+touch blank.js
+echo -e "//Create a model for your database for example with mongo create scheme ect" > blank.js
+
+cd ..
+
+cd requests
+touch create_blank.rest
+touch post_blank.rest
+touch get_blank.rest
+touch delete_blank.rest
+
+cd ..
+
+cd controllers
+touch blank.js
+echo -e "//create server request methods with your data and export as a variable to app.js" > blank.js
+
+cd ..
