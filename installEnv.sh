491ca688999c155ee5f3c7c369fbaa349f43bceb
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-12-15 19:48:01 +0200

    added indexjs text and appjs in backend added gitignore in backend

--- installEnv.sh
+++ installEnv.sh
@@ -128,3 +128,44 @@ touch blank.js
 echo -e "//create server request methods with your data and export as a variable to app.js" > blank.js
 
 cd ..
+
+touch .gitignore
+echo -e "node_modules
+.env" > .gitignore
+
+touch index.js
+echo -e "const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+    logger.info('Server running on port ', config.PORT )
+})" > index.js
+
+touch app.js
+
+echo -e "const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const blanksRouter = require('./controllers/blanks')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app" > app.js
